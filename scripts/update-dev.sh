7fa38ab039bb3dae735393fb28342d0e2cd66381
Norah C.IV <bartek.bober.koder@gmail.com>
2022-04-19 21:44:03 +0200

    update script

--- scripts/update-dev.sh
+++ scripts/update-dev.sh
@@ -6,4 +6,7 @@ echo 'pulling changes...'
 git checkout develop
 git pull
 
+echo 'Installing new packages...'
+yarn install
+
 echo 'now you have to run your apps manualy. To get list of app processess use yarn app-list'
