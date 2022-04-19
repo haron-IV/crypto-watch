4c872daf1397227e66694c91122cff090c679610
Norah C.IV <bartek.bober.koder@gmail.com>
2022-04-19 20:36:13 +0200

    fix vps script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'crypto-watch',
-      script: 'pm2 start ts-node -- -r tsconfig-paths/register ./index.ts',
+      script: 'yarn ts-node -r tsconfig-paths/register ./index.ts',
     },
   ],
 }
